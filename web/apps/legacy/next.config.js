e52c5ea4b80c3fb1ad8a3fee72fe2f0dd9b63673
Mycel-Ecosystem <mycel.FOSS@proton.me>
2026-07-12 20:31:17 +0300

    Delete web/apps/legacy directory

--- web/apps/legacy/next.config.js
+++
@@ -1,6 +0,0 @@
-const baseConfig = require("ente-base/next.config.base.js");
-
-module.exports = {
-    ...baseConfig,
-    transpilePackages: [...(baseConfig.transpilePackages ?? []), "pdfjs-dist"],
-};
